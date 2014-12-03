d76ffe1d31d2ef86739ef3fd3ff31a95752f3fd5
mobileflowllc <fieldforceapp@gmail.com>
2014-12-02 16:15:55 -0800

    Adding strings tests

--- examples/string.js
+++ examples/string.js
@@ -8,10 +8,115 @@
 
 var tessel = require('tessel');
 var digole12864 = require('../').use(tessel.port['D']);
+var async = require('async');
+
+console.log("Sending OLED set up command...");
 
 digole12864.on('ready', function(){
-	digole12864.clear(function(){
-		digole12864.string("Hello Tessel!");
-	});
+    console.log("Begin test...");
+
+    async.series([
+    function(callback){
+                  
+        digole12864.clear(function(){
+            digole12864.string("Hello Tessel!\n",callback);
+        });
+                  
+    },
+    function(callback){
+        setTimeout(function() {
+                   
+           digole12864.clear(function(){
+             digole12864.string("Hello Tessel again!!\n",callback);
+             });
+                   
+         }, 2000);
+    },
+    function(callback){
+        setTimeout(function() {
+                   
+           digole12864.clear(function(){
+             digole12864.stringXY(20,30,"Tessel over here!!\n",callback);
+             });
+                   
+         }, 2000);
+    },
+    function(callback){
+        setTimeout(function() {
+                   
+           digole12864.clear(function(){
+             digole12864.string("Underline\n");
+             digole12864.drawBox(3,15,67,1,callback);
+             });
+                   
+         }, 2000);
+    },
+    function(callback){
+        setTimeout(function() {
+                   
+           digole12864.clear(function(){
+             digole12864.setFont(6);
+             digole12864.string("Font Test No. 6\n",callback);
+             });
+                   
+         }, 2000);
+    },
+    function(callback){
+        setTimeout(function() {
+                   
+           digole12864.clear(function(){
+             digole12864.setFont(10);
+             digole12864.string("Font Test No. 10\n",callback);
+             });
+                   
+         }, 2000);
+    },
+    function(callback){
+        setTimeout(function() {
+                   
+           digole12864.clear(function(){
+             digole12864.setFont(18);
+             digole12864.string("Font Test No. 18\n",callback);
+             });
+                   
+         }, 2000);
+    },
+    function(callback){
+        setTimeout(function() {
+                   
+           digole12864.clear(function(){
+             digole12864.setFont(51);
+             digole12864.stringXY(0,58,"Font Test No. 51\n",callback);
+             });
+                   
+         }, 2000);
+    },
+    function(callback){
+        setTimeout(function() {
+                   
+           digole12864.clear(function(){
+             digole12864.setFont(120);
+             digole12864.stringXY(0,58,"Font Test No. 120\n",callback);
+             });
+                   
+         }, 2000);
+    },
+    function(callback){
+        setTimeout(function() {
+                   
+           digole12864.clear(function(){
+             digole12864.setFont(123);
+             digole12864.stringXY(1,63,"Font Test No. 123\n",callback);
+             });
+                   
+         }, 2000);
+    }
+  ], function(err, results){
+      if(err)
+         console.log("Error: "+err.message);
+  });
+
+
+               
 });
 
--- index.js
+++ index.js
@@ -11,6 +11,8 @@ var async = require('async');
 var util = require('util');
 var EventEmitter = require('events').EventEmitter;
 
+// Debug flag
+var DEBUG  = true
 
 // Screen is 128 by 64 bits.
 var LCD_WIDTH = 128
@@ -26,25 +28,36 @@ function Digole12864 (hardware, cb){
 
   self._lcdWrite = function(data, cb){
     var self = this;
-    self.uart.write(data);
+    self.uart.write(new Buffer(data,'ut8f'));
     if(cb)
-      cb(self);
+      cb();
   };
 
   async.series([
     function(callback){
       // Clear the display
+      if(DEBUG) console.log('Digole12864: init clear');
       self._lcdWrite('CL\n', callback);
     },
     function(callback){
       // Display welcome message
+      if(DEBUG) console.log('Digole12864: init message');
       self._lcdWrite('TTReady...\n', callback);
     }
   ],
   function(err, results){
-    self.emit('ready');
+    if(DEBUG) console.log('Digole12864: init ready');
+    // Emit the ready event when everything is set up
+    //   self.emit('ready');
+    // NOTE: Needed to wrap this within a setImmediate call, not sure why?
+    setImmediate(function emitReady() {
+        self.emit('ready');
+    });
+
+    if(err)
+        console.log('Digole12864: init err '+err.message);
     if(cb)
-      cb(err, self);
+       cb(null,self);
   });
 };
 
@@ -52,34 +65,89 @@ function Digole12864 (hardware, cb){
 util.inherits(Digole12864, EventEmitter);
 
 // Define the API
-Digole12864.prototype.gotoXY = function (x, y, cb){
-  var self = this;
-  async.series([
-    function(callback){
-      self._lcdWrite(0x80 | x, callback);
-    },
-    function(callback){
-      self._lcdWrite(0x40 | y, callback);
-    }
-  ], function(err, results){
-      cb(err);
-  });
-};
-
 Digole12864.prototype.character = function (character, cb){
   var self = this;
+  if(DEBUG) console.log("Write character ["+character+"]");
   self._lcdWrite(character);
+    if(cb)
+        cb();
 };
 
 Digole12864.prototype.string = function (data, cb){
   var self = this;
-  async.eachSeries(data, function(character, callback){
-    self.character(character, callback);
-  },
-  function(err, results){
-    if(cb)
-      cb(err); 
+  if(DEBUG) console.log("Write string: "+data);
+  self._lcdWrite('TT'+data);
+  if(cb)
+    cb();
+};
+
+Digole12864.prototype.stringXY = function (x,y, data, cb){
+  var self = this;
+  async.series([
+    function(callback){
+        if(DEBUG) console.log("Update position: "+util.format("ETP%d%d",x,y));
+        var buf = new Buffer(2);
+        buf.writeUInt8(x, 0);
+        buf.writeUInt8(y, 1);
+        self._lcdWrite('ETP'+buf.toString()+'\n',callback);
+
+    },
+    function(callback){
+      self.string(data, callback);
+        
+    }
+  ], function(err, results){
+      if(cb)
+        cb(err);
   });
+};
+
+Digole12864.prototype.drawBox = function (x, y, w, h, cb){
+  var self = this;
+  if(DEBUG) console.log("Draw box: "+util.format("%d,%d %d,%d",x,y,x+w,y+h));
+    var buf = new Buffer(4);
+    buf.writeUInt8(x, 0);
+    buf.writeUInt8(y, 1);
+    buf.writeUInt8(x+w, 2);
+    buf.writeUInt8(y+h, 3);
+
+  self._lcdWrite('FR'+buf.toString()+'\n',cb);
+};
+
+Digole12864.prototype.drawBoxFrame = function (x, y, w, h, cb){
+    var self = this;
+    if(DEBUG) console.log("Frame box: "+util.format("%d,%d %d,%d",x,y,x+w,y+h));
+    var buf = new Buffer(4);
+    buf.writeUInt8(x, 0);
+    buf.writeUInt8(y, 1);
+    buf.writeUInt8(x+w, 2);
+    buf.writeUInt8(y+h, 3);
+    
+    self._lcdWrite('DR'+buf.toString()+'\n',cb);
+};
+
+Digole12864.prototype.drawCircle = function (x, y, r, cb){
+    var self = this;
+    if(DEBUG) console.log("Draw circle: "+util.format("%d,%d %d",x,y,r));
+    var buf = new Buffer(4);
+    buf.writeUInt8(x, 0);
+    buf.writeUInt8(y, 1);
+    buf.writeUInt8(r, 2);
+    buf.writeUInt8(1, 3);       // Filled circle
+    
+    self._lcdWrite('CC'+buf.toString()+'\n',cb);
+};
+
+Digole12864.prototype.drawCircleFrame = function (x, y, r, cb){
+    var self = this;
+    if(DEBUG) console.log("Frame circle: "+util.format("%d,%d %d",x,y,r));
+    var buf = new Buffer(4);
+    buf.writeUInt8(x, 0);
+    buf.writeUInt8(y, 1);
+    buf.writeUInt8(r, 2);
+    buf.writeUInt8(0, 3);       // Filled circle
+    
+    self._lcdWrite('CC'+buf.toString()+'\n',cb);
 };
 
 Digole12864.prototype.bitmap = function (bitmapData, cb){
@@ -98,6 +166,25 @@ Digole12864.prototype.clear = function (cb){
   self._lcdWrite('CL\n', cb);
 };
 
+Digole12864.prototype.setMode = function(m, cb){
+    var self = this;
+    self._lcdWrite('SM'+m, cb);
+}
+
+Digole12864.prototype.setFont = function(f, cb){
+    var self = this;
+    var buf = new Buffer(1);
+    buf.writeUInt8(f, 0);
+    self._lcdWrite('SF'+buf.toString()+'\n',cb);
+}
+
+Digole12864.prototype.drawPixel = function(x, y, cb){
+    var self = this;
+    var buf = new Buffer(2);
+    buf.writeUInt8(x, 0);
+    buf.writeUInt8(y, 1);
+    self._lcdWrite('DP'+buf.toString()+'\n',cb);
+}
 
 // Every module needs a use function which calls the constructor
 function use (hardware, callback) {
@@ -105,6 +192,7 @@ function use (hardware, callback) {
 }
 
 // Export functions
+exports.Digole12864 = Digole12864;
 exports.use = use;
 
 
